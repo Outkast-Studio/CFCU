9cc0e03e44b0be6bdf2ea17220952ab5cf21556c
WilliamThomsonNZ <will@outkast.studio>
2024-11-25 16:16:18 +1300

    Add rates to the CMS

--- schemas/documents/index.ts
+++ schemas/documents/index.ts
@@ -2,6 +2,8 @@ import { SchemaTypeDefinition } from 'sanity'
 import post from './post'
 import subPage from './subPage'
 import location from './location'
+import rates from './rates'
+
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, subPage, location],
+  types: [post, subPage, location, rates],
 }
