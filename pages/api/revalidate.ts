1c69f052207175f5e0bdb89e6abec0c43ce562ca
WilliamThomsonNZ <will@outkast.studio>
2024-12-12 10:45:27 +1300

    build revalidation solution

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -53,7 +53,7 @@ export default async function revalidate(
       console.error(invalidId, { body })
       return res.status(400).send(invalidId)
     }
-    const staleRoutes = await queryStaleRoutes(req.body as any)
+    const staleRoutes = await queryStaleRoutes(body as any)
     console.log(staleRoutes)
     await Promise.all(staleRoutes.map((route) => res.revalidate(route)))
 
