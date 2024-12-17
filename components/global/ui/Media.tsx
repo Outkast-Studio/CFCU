99a7382ca9fccef862b0ab037978d5927a63effa
WilliamThomsonNZ <will@outkast.studio>
2024-12-17 14:39:13 +1300

    Test image loading solution for hero sections

--- components/global/ui/Media.tsx
+++ components/global/ui/Media.tsx
@@ -32,31 +32,35 @@ export default function MediaComponent({
   }, [isPlaying])
 
   if (stegaClean(media?.mediaType) === 'image' && media?.image) {
-    return (
-      <Image
-        //@ts-ignore
-        src={urlForImage(media?.image?.asset).width(2440).url()}
-        //@ts-ignore
-        alt={media?.image?.alt}
-        fill
-        priority={priority}
-        onLoadingComplete={(image) => image.classList.remove('opacity-0')}
-        className={clsx(
-          'object-cover w-full h-full',
-          !priority &&
-            'opacity-0 transition-all duration-300 ease-in-out-cubic',
-        )}
-      />
-      // <SanityImage
-      //   id={media?.image?.asset?._ref}
-      //   alt={media?.image?.alt}
-      //   baseUrl="https://cdn.sanity.io/images/uq2qrg8z/production/"
-      //   hotspot={media?.image?.hotspot}
-      //   className="object-cover w-full h-full"
-
-      //   priority={priority}
-      // />
-    )
+    if (priority) {
+      return (
+        <SanityImage
+          id={media?.image?.asset?._ref}
+          alt={media?.image?.alt}
+          baseUrl="https://cdn.sanity.io/images/uq2qrg8z/production/"
+          hotspot={media?.image?.hotspot}
+          className="object-cover w-full h-full"
+          priority={priority}
+        />
+      )
+    } else {
+      return (
+        <Image
+          //@ts-ignore
+          src={urlForImage(media?.image?.asset).width(2440).url()}
+          //@ts-ignore
+          alt={media?.image?.alt}
+          fill
+          priority={priority}
+          onLoadingComplete={(image) => image.classList.remove('opacity-0')}
+          className={clsx(
+            'object-cover w-full h-full',
+            !priority &&
+              'opacity-0 transition-all duration-300 ease-in-out-cubic',
+          )}
+        />
+      )
+    }
   } else if (stegaClean(media?.mediaType) === 'video' && media?.video) {
     return (
       <figure className={clsx('w-full h-full')}>
