e8250c9d055e12cb5bf91f25accd5836fbd24f85
WilliamThomsonNZ <will@outkast.studio>
2024-12-20 10:33:32 +1300

    add rate table gradient when its larger than max width

--- components/global/modules/RateTable.tsx
+++ components/global/modules/RateTable.tsx
@@ -2,13 +2,25 @@ import { RateTableType } from 'types/sanity'
 import { clsx } from 'clsx'
 import { formatDate } from 'utils'
 import { PortableText } from '@portabletext/react'
+import { useEffect, useRef } from 'react'
+import { useWindowSize } from '@/hooks/useWindowSize'
 
 const RateTable = ({ data }: { data: RateTableType }) => {
   const maxRows = data?.columns?.reduce(
     (max, column) => Math.max(max, column?.columnValues?.length),
     0,
   )
-
+  const { width } = useWindowSize(500)
+  const gradientRef = useRef<HTMLDivElement>(null)
+  const tableRef = useRef<HTMLTableElement>(null)
+  useEffect(() => {
+    if (!gradientRef.current || !tableRef.current) return
+    if (tableRef.current.offsetWidth + (width > 1024 ? 48 : 24) < width) {
+      gradientRef.current.style.opacity = '0'
+    } else {
+      gradientRef.current.style.opacity = '1'
+    }
+  }, [width])
   return (
     <section
       className={clsx(
@@ -30,48 +42,56 @@ const RateTable = ({ data }: { data: RateTableType }) => {
           Date Updated {formatDate(data?._updatedAt)}
         </h4>
       </div>
-      <div
-        className={clsx(
-          'overflow-x-auto mt-[29px] px-[24px]',
-          'lg:mt-[44px] lg:px-[48px]',
-          'xl:px-[0px]',
-        )}
-      >
-        <table className="w-full">
-          <thead>
-            <tr>
-              {data?.columns?.map((column, index) => (
-                <th
-                  key={index}
-                  className={clsx(
-                    'bg-lavender text-left w-h6 text-white py-[14px] pl-[20px] min-w-[300px]',
-                    'lg:pt-[23px] lg:pb-[16px] lg:pl-[31px] lg:w-h6-desktop',
-                  )}
-                >
-                  {column?.columnTitle}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {Array.from({ length: maxRows }).map((_, rowIndex) => (
-              <tr key={rowIndex}>
-                {data?.columns?.map((column, colIndex) => (
-                  <td
-                    key={colIndex}
+      <div className={clsx('relative h-fit')}>
+        <div
+          ref={gradientRef}
+          className={clsx(
+            'absolute right-0 top-0 w-[70px] h-full rateTableGradient z-[2]',
+          )}
+        ></div>
+        <div
+          className={clsx(
+            'overflow-x-auto mt-[29px] px-[24px] relative',
+            'lg:mt-[44px] lg:px-[48px]',
+            'xl:px-[0px]',
+          )}
+        >
+          <table className="w-full" ref={tableRef}>
+            <thead>
+              <tr>
+                {data?.columns?.map((column, index) => (
+                  <th
+                    key={index}
                     className={clsx(
-                      'pt-[16px] pb-[18px] pl-[20px] border-b-[1px] border-black/20 w-paragraph-s-desktop',
-                      rowIndex % 2 === 0 ? 'bg-[#EDEDED]/20' : 'bg-white',
-                      'lg:pl-[31px] lg:pb-[20px] lg:pt-[16px] lg:w-paragraph-l-desktop',
+                      'bg-lavender text-left w-h6 text-white py-[14px] pl-[20px] min-w-[300px]',
+                      'lg:pt-[23px] lg:pb-[16px] lg:pl-[31px] lg:w-h6-desktop',
                     )}
                   >
-                    {column?.columnValues[rowIndex] || '\u00A0'}
-                  </td>
+                    {column?.columnTitle}
+                  </th>
                 ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {Array.from({ length: maxRows }).map((_, rowIndex) => (
+                <tr key={rowIndex}>
+                  {data?.columns?.map((column, colIndex) => (
+                    <td
+                      key={colIndex}
+                      className={clsx(
+                        'pt-[16px] pb-[18px] pl-[20px] border-b-[1px] border-black/20 w-paragraph-s-desktop',
+                        rowIndex % 2 === 0 ? 'bg-[#EDEDED]/20' : 'bg-white',
+                        'lg:pl-[31px] lg:pb-[20px] lg:pt-[16px] lg:w-paragraph-l-desktop',
+                      )}
+                    >
+                      {column?.columnValues[rowIndex] || '\u00A0'}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
       {data?.tableNotes && (
         <div
