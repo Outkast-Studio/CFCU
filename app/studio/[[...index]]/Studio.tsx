ef037ad7dd0f2761d0982c2c306a563eb2250f59
WilliamThomsonNZ <will@outkast.studio>
2025-01-13 11:11:07 +1300

    remove number swatches from input

--- app/studio/[[...index]]/Studio.tsx
+++ app/studio/[[...index]]/Studio.tsx
@@ -2,7 +2,30 @@
 
 import { NextStudio } from 'next-sanity/studio'
 import config from 'sanity.config'
+import { StudioProvider, StudioLayout } from 'sanity'
+import { createGlobalStyle } from 'styled-components'
 
 export default function Studio() {
-  return <NextStudio config={config} />
+  const GlobalStyles = createGlobalStyle`
+  /* Chrome, Safari, Edge, Opera */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  input[type=number] {
+    -moz-appearance: textfield;
+  }
+`
+  return (
+    <NextStudio config={config}>
+      <StudioProvider config={config}>
+        <GlobalStyles />
+        {/* Put components here and you'll have access to the same React hooks as Studio gives you when writing plugins */}
+        <StudioLayout />
+      </StudioProvider>
+    </NextStudio>
+  )
 }
