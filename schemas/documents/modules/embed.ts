b56d8b570f79417a45f20641c98fe394ad20bdb2
WilliamThomsonNZ <will@outkast.studio>
2025-01-16 14:38:36 +1300

    Add embded title

--- schemas/documents/modules/embed.ts
+++ schemas/documents/modules/embed.ts
@@ -6,6 +6,13 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      description: 'A title to indentify the embed',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
       name: 'scriptsForBody',
       title: 'Scripts for Body',
       description: 'Scripts to be added to the body tag',
@@ -19,10 +26,13 @@ export default defineType({
     }),
   ],
   preview: {
-    select: {},
-    prepare() {
+    select: {
+      title: 'title',
+    },
+    prepare({ title }) {
       return {
         title: 'Embed',
+        subtitle: title,
       }
     },
   },
