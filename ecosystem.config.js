0e03559946c0a7109e89053e682ce3a20e5d8ee4
Toni Medina <tonimedina.me@gmail.com>
2025-12-05 08:11:38 -0500

    build: upsun config updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,27 +4,17 @@ module.exports = {
       name: 'cfcu-nextjs',
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+        PORT: process.env.PORT || 3000,
+      },
+      max_memory_restart: '1G',
+      error_file: '$PLATFORM_APP_DIR/.pm2/logs/error.log',
+      out_file: '$PLATFORM_APP_DIR/.pm2/logs/out.log',
+      merge_logs: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
     },
   ],
 }
-
-// module.exports = {
-//   apps: [
-//     {
-//       name: 'cfcu-nextjs',
-//       script: 'node_modules/next/dist/bin/next',
-//       args: 'start',
-//       instances: 1,
-//       exec_mode: 'cluster',
-//       env: {
-//         NODE_ENV: 'production',
-//         PORT: process.env.PORT || 3000,
-//       },
-//       max_memory_restart: '1G',
-//       error_file: '/tmp/pm2-error.log',
-//       out_file: '/tmp/pm2-out.log',
-//       merge_logs: true,
-//       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-//     },
-//   ],
-// }
