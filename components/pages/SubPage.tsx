256f07a492389cc0128e8c2ebce208346d8e8e9a
Toni Medina <tonimedina.me@gmail.com>
2025-12-09 11:30:43 -0500

    fix: vercel not building

--- components/pages/SubPage.tsx
+++ components/pages/SubPage.tsx
@@ -6,7 +6,8 @@ import { SubPageType } from 'types/sanity'
 import ModuleFactory from '@/components/global/modules/ModuleFactory'
 import { renderModule } from '@/components/global/modules/ModuleFactory'
 import SubPageHero from '@/components/global/modules/subPageHero'
-import SubpageLinkList from '@/components/global/ui/SubpageLinkList'
+
+import SubpageLinkList from './../../components/global/ui/SubpageLinkList'
 
 const SubPage = ({
   data,
